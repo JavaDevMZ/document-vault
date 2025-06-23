9b0a99348019dabe4cc5bb8dc7047b27c8d07362
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2025-06-23 08:32:54 +0200

    Add ORM annotations

--- DocVault.API/Models/Document.cs
+++ DocVault.API/Models/Document.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DocVault.API.Models;
 
+[Table("document")]
 public class Document
 {
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string FilePath { get; set; } = "";
-    public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
 
+    [Required]
+    [Column("name")]
+    public string Name { get; set; } = "";
+
+    [Column("file_path")]
+    public string FilePath { get; set; } = "";
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
 }
